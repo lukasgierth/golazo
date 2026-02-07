5cd5ad44d9e4fb52db0b315264c6664853439e45
Juanma Roca <juanmaxroca@gmail.com>
2026-02-06 22:08:03 -0500

    chore[nix]: bump version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {pkgs ? import <nixpkgs> {}, ...}:
 pkgs.buildGoModule {
   pname = "golazo";
-  version = "0.14.0";
+  version = "0.21.0";
   vendorHash = "sha256-hPrWqDmsCjAnstKIV8W5tqCR4i6uRpnFIZWMr4OKEUo=";
 
   subPackages = ["."];
